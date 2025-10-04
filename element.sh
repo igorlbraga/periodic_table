60b73f4c5d4c4bc834d805dc1d1c764999816652
igorlbraga <hakcof@outlook.com>
2025-10-04 15:13:51 -0400

    test: update column names and data types in queries

--- element.sh
+++ element.sh
@@ -5,7 +5,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 # Check if argument is provided
 if [[ -z $1 ]]
 then
-  echo "Please provide an element as an argument."
+  echo "Please provide an element as an argument." 
   exit
 fi
 
